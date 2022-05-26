7eb32ca640885128b9b5050567ac31c263267e0c
Aryan0807 <aryan.goyal@iitg.ac.in>
2022-05-26 20:34:24 +0530

    indexing

--- src/main/java/com/example/url_shortner/entity/Url.java
+++ src/main/java/com/example/url_shortner/entity/Url.java
@@ -1,11 +1,19 @@
 package com.example.url_shortner.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Url {
     @Id
     private String id;
+
+    @Indexed(name = "org_url", direction = IndexDirection.ASCENDING)
     private String orgUrl;
+
+    @Indexed(name= "new_url", direction = IndexDirection.ASCENDING)
     private String  newUrl;
 
     public Url(String orgUrl, String newUrl ) {
